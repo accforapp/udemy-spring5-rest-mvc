e1a9f3ca53a8522d2b37b8823996d525399b3a67
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2022-02-02 22:20:08 +0200

    version 1.0.1
    added entity for category;
    added jpa repository for category;
    added dto for category.

---
+++ src/main/java/udemy/spring5/restmvc/api/v1/model/CategoryDTO.java
@@ -0,0 +1,11 @@
+package udemy.spring5.restmvc.api.v1.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+  private Long id;
+  private String name;
+}
---
+++ src/main/java/udemy/spring5/restmvc/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package udemy.spring5.restmvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import udemy.spring5.restmvc.domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Long, Category> {
+}
