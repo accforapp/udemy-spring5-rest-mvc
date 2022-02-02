0d654c6397166492f8c15f72f9c070b3ead3a350
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2022-02-02 22:39:21 +0200

    version 1.0.2
    fixed category jpa generic;
    added bootstrap.

--- src/main/java/udemy/spring5/restmvc/repositories/CategoryRepository.java
+++ src/main/java/udemy/spring5/restmvc/repositories/CategoryRepository.java
@@ -3,5 +3,5 @@ package udemy.spring5.restmvc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import udemy.spring5.restmvc.domain.Category;
 
-public interface CategoryRepository extends JpaRepository<Long, Category> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
