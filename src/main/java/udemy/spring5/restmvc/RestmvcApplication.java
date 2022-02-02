d274adaa4153c945a40dc8586c52f7b1fb66ec0a
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2022-02-02 22:10:43 +0200

    version 1.0.0
    creating project

---
+++ src/main/java/udemy/spring5/restmvc/RestmvcApplication.java
@@ -0,0 +1,13 @@
+package udemy.spring5.restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/spring5/restmvc/RestmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.spring5.restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
